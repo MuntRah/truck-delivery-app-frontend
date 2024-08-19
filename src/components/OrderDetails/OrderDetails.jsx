dc3bc7412f4cdf34aeb636d05b1e8e035459a217
sayedalali1 <sayedalali1@gmail.com>
2024-08-19 14:49:08 +0300

    added buttons for update & delete (both non functional)

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -37,6 +37,10 @@ const OrderDetails = (props) => {
         <p>Vehicle type : {order.vehicle}</p>
         <p>Status : {order.orderStatus}</p>
         <p>Price :{order.price}</p>
+        <button id="update" type="update">UPDATE</button>
+        <button id="delete" type="delete">DELETE</button>
+
+
         {/* <CustomerDate name={order.customer.username} date={order.createdAt}/> */}
       </header>
       <p>{order.text}</p>
