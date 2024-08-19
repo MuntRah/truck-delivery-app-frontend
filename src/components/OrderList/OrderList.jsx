a36589881e1691881669b790b11c20c2ad3870e7
sayedalali1 <sayedalali1@gmail.com>
2024-08-19 12:53:00 +0300

    troubleshooting orderservice/orderlist

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -1,5 +1,21 @@
-const OrderList = (props) => {
-  return <main>Order List</main>;
-};
 
+const OrderList = ({ orders }) => {
+  if (!orders || orders.length === 0) {
+    return <main>There are no orders.</main>;
+  }
+  return (
+    <main>
+      {orders.map((order) => (
+        <Link key={order._id} to={`/orders/${order._id}`}>
+          <div>
+            <h2>{order.from}</h2>
+            <h2>{order.to}</h2>
+            <h2>{order.price}</h2>
+            <h2>{order.status}</h2>
+          </div>
+        </Link>
+      ))}
+    </main>
+  );
+};
 export default OrderList;
