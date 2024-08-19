9a4df59174a3b06d4e764a64b30e181b1a577bb6
MuntRah <me.alrahim.02@gmail.com>
2024-08-19 14:23:46 +0300

    i edit the orderlist and add the details on orderDetails file

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -9,37 +9,38 @@ import orderService from "../../services/orderService";
 // import CustomerDate from "../common/CustomerDate";
 // import DescriptionForm from '../DescriptionForm/DescriptionForm';
 
-
 const OrderDetails = (props) => {
   const { orderId } = useParams();
   const [order, setOrder] = useState(null);
 
-  useEffect(()=>{
-    async function getOrder(){
-      const orderData = await orderService.show(orderId)
-      setOrder(orderData)
+  useEffect(() => {
+    async function getOrder() {
+      const orderData = await orderService.show(orderId);
+      setOrder(orderData);
     }
-    getOrder()
-  },[orderId])
+    getOrder();
+  }, [orderId]);
 
-  if(!order){
-    return <main><h3>Loading...</h3></main>
+  if (!order) {
+    return (
+      <main>
+        <h3>Loading...</h3>
+      </main>
+    );
   }
 
   return (
     <main>
       <header>
-      <h2>From :{order.from}</h2>
-              <h2>To: {order.to}</h2>
-              <p>Vehicle type : {order.vehicle}</p>
-              <p>Status : {order.orderStatus}</p>
-              <p>Price :{order.price}</p>
+        <h2>From :{order.from}</h2>
+        <h2>To: {order.to}</h2>
+        <p>Vehicle type : {order.vehicle}</p>
+        <p>Status : {order.orderStatus}</p>
+        <p>Price :{order.price}</p>
         {/* <CustomerDate name={order.customer.username} date={order.createdAt}/> */}
       </header>
       <p>{order.text}</p>
-      <section>
-
-      </section>
+      <section></section>
     </main>
   );
 };
