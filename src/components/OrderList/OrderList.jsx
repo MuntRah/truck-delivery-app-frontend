7c180c32d5f686cf0a17459ce065557ad10bce2c
MuntRah <me.alrahim.02@gmail.com>
2024-08-19 14:28:56 +0300

    i edit the orderlist and the orderDetails

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -9,8 +9,8 @@ const OrderList = ({ orders }) => {
         <section>
           <ul>
             <li>
-              <h2>From :{order.from}</h2>
-              <h2>To: {order.to}</h2>
+              <h3>From :{order.from}</h3>
+              <h3>To: {order.to}</h3>
               <p>Price :{order.price}</p>
               <p>Status : {order.orderStatus}</p>
               <p>Vehicle type : {order.vehicle}</p>
